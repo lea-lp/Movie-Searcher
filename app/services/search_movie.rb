3f49ffbf2fc4724ad6cf192299dead28e16099a3
Clem Baldy <clemoun@yahoo.fr>
2018-11-08 16:37:32 +0100

    service SearchMovie fonctionnel avec nom et date

---
+++ app/services/search_movie.rb
@@ -0,0 +1,26 @@
+class SearchMovie
+
+  def initialize(string)
+    @string = string
+    @result = Hash.new
+    
+  end
+
+  def perform
+    Tmdb::Api.key(ENV['API_KEY'])
+
+    @movies = Tmdb::Movie.find(@string)
+
+    @movies.each_with_index do |movie, index|
+      if index == 19
+        break
+      end
+      name = movie.title
+      release = movie.release_date
+
+      @result[movie.id] = {name: name, release_date: release}
+      puts @result[movie.id]
+    end
+    return @result
+  end
+end
